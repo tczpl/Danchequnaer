4242923c9241dd1da9792d085faf097e0423791d
tczpl <tczpl@163.com>
2016-06-29 03:18:30 +0800

    1
    1

---
+++ success.php
@@ -0,0 +1,34 @@
+﻿<!doctype html>
+<html lang="en">
+<body>
+
+<?php 
+echo
+"<html>
+<head>
+	<meta charset='utf-8'>
+	<title>单车去哪儿</title>
+	<link rel='stylesheet' href='bootstrap\css\bootstrap.css' type='text/css' />
+</head>
+<body>
+<div style='background:#009cff; width:100%; height:1000px;'>
+<center>
+	<div class=\"btn-group\">
+		<button type=\"button\" class=\"btn btn-primary\" onClick=\"location.href='index.php'\">单车去哪儿</button>
+		<button type=\"button\" class=\"btn btn-success\" onClick=\"location.href='zuixin.php'\">最新丢车</button>
+		<button type=\"button\" class=\"btn btn-info\" onClick=\"location.href='tongji.php'\">丢车统计</button>
+		<button type=\"button\" class=\"btn btn-danger\" onClick=\"location.href='ditu.php'\">防偷地图</button>
+        <button type=\"button\" class=\"btn btn-warning\" onClick=\"location.href='baogao.html'\">丢车报告</button>
+	</div>
+</center>
+<br><br><br><br>
+<div class='wodejuzhong'>
+<img src =\"success.png\" align =\"center\"> <br>
+<center>
+<a href='youxi.html'><font size='+1' color=ffffff><u>来玩彩蛋游戏释放愤怒吧</u></font></a>
+</center>
+</div>
+</body>
+</html>"; 
+
+?>
